65afde5123ffc65a31f84e01162b8d13de771fa8
QuanNguyen-da <Lequan100403@gmail.com>
2025-01-08 13:46:31 +0700

    Add files via upload

---
+++ Crawling_Mogi.py
@@ -0,0 +1,53 @@
+import requests
+import pandas as pd
+from bs4 import BeautifulSoup
+
+data = []
+
+for page in range(1, 683):
+    url = f'https://mogi.vn/da-nang/mua-nha-dat?cp={page}'
+    response = requests.get(url)
+    
+    if response.status_code == 200:
+        print(f'Truy cập trang thứ {page} thành công')
+        soup = BeautifulSoup(response.text, 'html.parser')
+        houses = soup.find_all('div', class_='prop-info')
+        dates = soup.find_all('div', class_='prop-extra')
+
+        for house in houses:
+            link = house.find('a', class_="link-overlay")['href']
+            title = house.find('h2', class_="prop-title").text.strip()
+            addr = house.find('div', class_="prop-addr").text.strip()
+            price = house.find('div', class_="price").text.strip()
+            attributes = [li.text.strip() for li in house.find('ul', class_='prop-attr').find_all('li')] if house.find('ul', class_='prop-attr') else ["N/A", "N/A"]
+            dientich = attributes[0].strip()
+            bedroom = attributes[1].strip()
+            bathroom = attributes[2].strip()
+
+            # Kiểm tra ngày và gắn nó vào cùng dòng dữ liệu
+            day = None  # Nếu không có ngày thì để None
+            for date in dates:
+                day = date.find('div', class_="prop-created").text.strip() if date else None
+
+            # Thêm tất cả dữ liệu vào trong một dòng
+            data.append([link, title, addr, dientich, bedroom, bathroom, price, day])
+
+    else:
+        # Mã trạng thái và ý nghĩa nếu không thành công
+        status_code_meanings = {
+            400: "Bad Request: Yêu cầu không hợp lệ.",
+            401: "Unauthorized: Cần xác thực.",
+            403: "Forbidden: Truy cập bị từ chối.",
+            404: "Not Found: Trang không tìm thấy.",
+            500: "Internal Server Error: Lỗi máy chủ.",
+            503: "Service Unavailable: Dịch vụ không khả dụng."
+        }
+        
+        # Lấy ý nghĩa của mã trạng thái nếu có
+        meaning = status_code_meanings.get(response.status_code, "Mã trạng thái không xác định.")
+        
+        print(f"Không thể truy cập, mã trạng thái: {response.status_code} - {meaning}")
+
+# Đổi tên cột để bao gồm "day" ở cuối
+df = pd.DataFrame(data, columns=['link', 'title', 'addr', 'dientich', 'bedroom', 'bathroom', 'price', 'day'])
+df.to_csv('mogiday.csv', index=False)
---
+++ Pre-processing.sql
@@ -0,0 +1,154 @@
+﻿select * from Mogiok
+
+--Thêm cột tiền tỷ
+alter table Mogiok
+add billion numeric(15,0)
+
+--Thêm cột tiền triệu
+alter table Mogiok
+add million numeric(15,0)
+
+--Thêm cột đơn vị nghìn
+alter table Mogiok
+add thousand numeric(15,0)
+
+---Thêm cột price sau khi xử lý
+alter table mogiok
+add Price_Final numeric(15,0)
+
+select * from mogiok where price like N'%nghìn%'
+--cập nhật giá trị cho cột đơn vị nghìn
+UPDATE mogiok
+SET thousand = CAST(SUBSTRING(price, CHARINDEX(N'triệu', price) + 6, 3) AS NUMERIC(15, 3))
+WHERE price LIKE N'%nghìn%';
+UPDATE mogiok
+SET thousand = 0
+WHERE price not LIKE N'%nghìn%';
+
+EXEC sp_columns mogiok;
+
+
+
+-- Cập nhật giá trị cho cột tiền tỷ
+update mogiok
+set billion =  case when price not like N'%tỷ%' and price like N'%triệu%' then 0
+					when price like N'%tỷ%'   then cast(substring(price, 1, CHARINDEX(N'tỷ',price) -1) as numeric(15,0))
+
+				end
+
+delete from mogiok
+where substring(price,charindex(N'tỷ',price)+3,3) like N'%7 t%'
+
+--Cập nhật giá trị cho cột tiền triệu
+update mogiok
+set million =  case when price not like N'%triệu%' 					then 0
+when price like N'%triệu%' and price like N'%tỷ%' 
+					then cast(substring(price,charindex(N'tỷ',price)+3,3) as numeric(15,0))
+					when price  like N'%triệu%' and price not like N'%tỷ%'  
+					then cast(substring(price,1,charindex(N'triệu',price)-1) as numeric(15,0))
+end
+
+select * from mogiok
+UPDATE mogiok
+SET billion = billion * 1000000000;
+
+
+UPDATE mogiok
+SET million = million * 1000000;
+
+UPDATE mogiok
+SET thousand = thousand * 1000;
+
+--Quy đổi giá trị triệu thành tỷ 
+update mogi 
+set Price_Final=billion+million+thousand
+
+select * from mogiok
+
+ 
+--Cập nhật giá trị cho cột Price_Final
+update mogiok
+set price_final = (billion+million+thousand)
+
+select bedroom, len(bedroom) from mogiok
+
+--Cập nhật giá trị cho cột số phòng ngủ
+update mogiok
+set bedroom =   cast(substring(BedRoom, 1, charindex('PN',BedRoom)-1) as int)
+
+select bedroom from mogiok where bedroom not like N'%PN%'
+
+delete from mogiok
+where bedroom not like N'%PN%'
+
+
+
+--Cập nhật giá trị cho cột số phòng WC
+update mogiok
+set Bathroom = cast(substring(bathroom,1,charindex('WC',BathRoom)-1) as int)
+
+--Cập nhật lại dientich 
+update mogiok
+set Area =cast(substring(Area,1,charindex('m2',Area)-1) as int)
+
+--Thêm cột Address_final
+alter table mogiok
+add address_final nvarchar(100)
+
+select * from mogiok
+--Lấy ra thành phố từ cột Address
+update mogiok
+set address_final  = SUBSTRING(address, CHARINDEX('Quận', address) + 5, CHARINDEX(',', address) - (CHARINDEX('Quận', address) + 5))
+
+
+SELECT COLUMN_NAME, DATA_TYPE
+FROM INFORMATION_SCHEMA.COLUMNS
+WHERE TABLE_NAME = 'mogiok' AND COLUMN_NAME = 'Date';
+
+select date from mogiok
+WHERE year(date) =2025;
+
+UPDATE mogiok
+SET date = 
+     SUBSTRING([Date], 7, 4) + '-' + SUBSTRING([Date], 4, 2) + '-' + SUBSTRING([Date], 1, 2)
+WHERE ISDATE([Date]) = 0;
+
+
+
+
+
+select * from mogiok
+
+SELECT DISTINCT address_final
+FROM mogiok;
+
+update  mogiok
+set address_final=N'Hòa Vang'
+where address_final like N'n Hoà Vang'
+
+alter table mogiok
+add  Address_ma_hoa int
+
+UPDATE mogiok
+SET address_final = REPLACE(address_final, ' ', '')
+WHERE address_final LIKE '% %';
+
+UPDATE mogiok
+SET address_final = REPLACE(REPLACE(REPLACE(address_final, 'HòaVang', 'Hòa Vang'), 'HảiChâu', 'Hải Châu'), 'LiênChiểu', 'Liên Chiểu')
+-- Tiếp tục với các từ khác nếu có
+WHERE address_final IN ('HòaVang', 'HảiChâu', 'LiênChiểu', 'NgũHànhSơn', 'CẩmLệ', 'SơnTrà', 'ThanhKhê');
+
+UPDATE mogiok
+SET Address_ma_hoa = CASE 
+    WHEN address_final = N'HảiChâu' THEN 1
+    WHEN address_final = N'LiênChiểu' THEN 2
+    WHEN address_final = N'NgũHànhSơn' THEN 3
+    WHEN address_final = N'CẩmLệ' THEN 4
+    WHEN address_final = N'SơnTrà' THEN 5
+    WHEN address_final = N'ThanhKhê' THEN 6
+    WHEN address_final = N'Hòa Vang' THEN 7
+    ELSE NULL
+
+END;
+
+select * from mogiok
